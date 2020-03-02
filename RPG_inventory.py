7f4d00377d60de2f33e2483814bbce1492f6733c
katepepper <60437068+katepepper@users.noreply.github.com>
2020-03-02 09:18:35 -0600

    Add files via upload

---
+++ RPG_inventory.py
@@ -0,0 +1,5 @@
+# A list of all of the suspects of the murder
+suspects = ['Evelyn Finch', 'George Brown', 'Madeline Brown', 'Stephen Turner']
+
+# A list of all of the potential murder weapons
+weapons= ['knife', 'poison', 'gun', 'crowbar', 'axe', 'rope']
