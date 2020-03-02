e2c75a845719e1e3732c13bc336f83cdbd7329ac
katepepper <60437068+katepepper@users.noreply.github.com>
2020-03-02 13:14:35 -0600

    Add files via upload

--- RPG_inventory.py
+++ RPG_inventory.py
@@ -1,5 +1,8 @@
 # A list of all of the suspects of the murder
 suspects = ['Evelyn Finch', 'George Brown', 'Madeline Brown', 'Stephen Turner']
-
+suspects.append('Chantal Steele')
 # A list of all of the potential murder weapons
-weapons= ['knife', 'poison', 'gun', 'crowbar', 'axe', 'rope']
+weapons = ['knife', 'poison', 'gun', 'crowbar', 'axe', 'rope']
+
+# A list of potential rooms where the murder took place
+rooms = ['library', 'kitchen', 'bathroom', 'dining room', 'bedroom', 'office']
