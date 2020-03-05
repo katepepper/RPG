5bd7ca9ea55d6294c54faa6245a67687483740e3
katepepper <60437068+katepepper@users.noreply.github.com>
2020-03-05 09:27:21 -0600

    Add files via upload

--- RPG_inventory.py
+++ RPG_inventory.py
@@ -1,8 +1,18 @@
 # A list of all of the suspects of the murder
 suspects = ['Evelyn Finch', 'George Brown', 'Madeline Brown', 'Stephen Turner']
+print(suspects)
 suspects.append('Chantal Steele')
+suspects.insert(2, 'Justin Jones')
+print(suspects)
 # A list of all of the potential murder weapons
-weapons = ['knife', 'poison', 'gun', 'crowbar', 'axe', 'rope']
+weapons = ['knife', 'poison', 'gun', 'crowbar', 'axe', 'rope', 'bat','rifle']
+print(weapons)
+del weapons[-1]
+print(weapons)
 
 # A list of potential rooms where the murder took place
 rooms = ['library', 'kitchen', 'bathroom', 'dining room', 'bedroom', 'office']
+print(rooms)
+murder_room = rooms.pop()
+print(f"The murder took place in the {murder_room.title()}.")
+print(rooms)
